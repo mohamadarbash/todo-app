143c2f9f342a9cf05e0578be914eedc0ebb805d4
mohamad arbash <mohamad.arbash@gmail.com>
2019-01-06 21:28:34 +0100

    add get API

--- server/server.js
+++ server/server.js
@@ -22,6 +22,12 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find().then((docs) => {
+		res.send({docs});
+	}, (err) => res.status(400).send(err));
+});
+
 app.listen(3000, () => {
 	console.log('Started on Port 3000');
 });
